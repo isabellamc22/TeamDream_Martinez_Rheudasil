36b86df26f0d01aeccbf4814cb2cb755db1b51bb
isabellamc22 <149627481+isabellamc22@users.noreply.github.com>
2023-11-12 15:00:41 -0600

    Update Beauty_and_the_Beast.ino

--- Beauty_and_the_Beast/Beauty_and_the_Beast.ino
+++ Beauty_and_the_Beast/Beauty_and_the_Beast.ino
@@ -1,12 +1,18 @@
 #include <Servo.h>
 
-Servo myServo; //servo variable
-Servo myServo2
-const int buttonPin = 2;
+Servo myservo; //servo variable
+Servo myservo2
+const int buttonPin = 1;
+const int buttonPin2 = 2;
+const int buttonPin3 = 3;
 
 void setup() {
   // put your setup code here, to run once:
+  myservo.attach(10);
+  myservo2.attach(9);
   pinMode(buttonPin, INPUT);
+  pinMode(buttonPin2, INPUT);
+  pinMode(buttonPin3, INPUT);
 }
 
 void loop() {
@@ -21,8 +27,11 @@ void loop() {
   }
 
   //SCENE #2
-  if ((digitalRead (buttonpin2) == HIGH)&&(digitalRed (buttonpin3) == HIGH)){
+  if ((digitalRead (buttonpin2) == HIGH)&&(digitalRed (buttonpin3) == HIGH)) {
     //belle and beast will dance, circle surface will spin
     myservo2.write(180);
+    myservo2.write(0);
+    myservo2.write(180);
+    myservo2.write(0);
   }
 }
