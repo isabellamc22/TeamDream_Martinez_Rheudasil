e132ff45b4bedf0da2ddbe6dd3626345e0f191cd
isabellamc22 <149627481+isabellamc22@users.noreply.github.com>
2023-11-15 04:10:30 -0600

    Update Beauty_and_the_Beast.ino

--- Beauty_and_the_Beast/Beauty_and_the_Beast.ino
+++ Beauty_and_the_Beast/Beauty_and_the_Beast.ino
@@ -1,37 +1,59 @@
 #include <Servo.h>
 
 Servo myservo; //servo variable
-Servo myservo2
+//Servo myservo2;
+int pos = 0;
 const int buttonPin = 2;
-const int buttonPin2 = 4;
-const int buttonPin3 = 6;
+//const int buttonPin2 = 4;
+const int servoPin = 12;
+//const int servoPin2 = 10;
+int buttonState = 0;
+//int buttonState2 = 0;
+int previousButtonState;
+//int previousButtonState2;
 
+
+unsigned long currentTime;
+unsigned long previousTime = 0;
+int timerLength = 2000;
 void setup() {
   // put your setup code here, to run once:
-  myservo.attach(12);
-  myservo2.attach(10);
+  myservo.attach(servoPin);
+ // myservo2.attach(servoPin2);
+  myservo.write(0);
+  //myservo2.write(0);
   pinMode(buttonPin, INPUT);
-  pinMode(buttonPin2, INPUT);
-  pinMode(buttonPin3, INPUT);
+ // pinMode(buttonPin2, INPUT);
+  pinMode(servoPin, OUTPUT);
+ // pinMode(servoPin2, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  
-  // SCENE #1: belle approaching rose
-  if (digitalRead(buttonpin) == HIGH){
-    //rose will spin from pretty to withered
-    myservo.write(180);
-  } else {
-    myservo.write(0);
-  }
 
-  //SCENE #2
-  if ((digitalRead (buttonpin2) == HIGH)&&(digitalRed (buttonpin3) == HIGH)) {
-    //belle and beast will dance, circle surface will spin
-    myservo2.write(180);
-    myservo2.write(0);
-    myservo2.write(180);
-    myservo2.write(0);
+  // SCENE #1: belle approaching rose
+  buttonState = digitalRead(buttonPin);
+
+  if (previousButtonState != buttonState){
+    //rose will spin from pretty to withered
+    if(buttonState == HIGH){
+      myservo.write(180);
+    } else {
+      myservo.write(0);
+    }
   }
+  previousButtonState = buttonState;
+
+  //SCENE #3
+//  buttonState2 = digitalRead(buttonPin2);
+  
+//  if (previousButtonState2 != buttonState2){
+//    //belle and beast will dance!
+//    if(buttonState2 == HIGH){
+//      myservo2.write(180);
+//    } else {
+//      myservo2.write(0);
+//    }
+//  }
+//  previousButtonState2 = buttonState2;
 }
